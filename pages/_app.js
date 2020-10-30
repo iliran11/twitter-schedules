3ded63fca36f8026cdb20c730a62a198f69b39d5
Liran <iliran11@gmail.com>
2020-10-30 06:37:26 +0200

    Add metropolis font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../style/index.scss'
+import Head from "next/head";
+import "../style/index.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          media="screen"
+          href="https://fontlibrary.org/face/metropolis"
+          type="text/css"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
